5fd64a4b436695b26fbfef15aeef08f6ba6b04b9
flore <flore@EMYLIS>
2024-07-22 13:46:25 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding2429841/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBinding2429841
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
